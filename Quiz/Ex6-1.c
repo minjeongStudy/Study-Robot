db119cf01eb678188ca804cefd56a504a3326836
minjeongStudy <minjeongstudy@gmail.com>
2025-11-03 12:01:47 +0900

    while문
    1~10까지 10~1까지

---
+++ Quiz/Ex6-1.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+/*	// 1 ~ 10 까지 출력하는 while 문을 작성하세요!
+
+	int i = 0;
+
+	while ( i < 10) {
+	//	i += 1;
+	//	printf("%d " , i);
+	//	i++;
+	//	printf("%d ", i);
+		printf("%d ", ++i);
+	}
+*/
+
+	int i = 10;
+
+	while(i > 0) {
+		printf("%d ", i--);
+
+	}
+
+
+
+	return 0;
+}
